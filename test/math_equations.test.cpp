e72db6c5152a15f862d168e5bc46b431cd22a2ce
Johannes Stricker <johannesstricker@gmx.net>
2021-07-20 07:11:59 +0200

    Simplify tests

--- test/math_equations.test.cpp
+++ test/math_equations.test.cpp
@@ -26,75 +26,31 @@ TEST_CASE("stringToMarkers") {
   REQUIRE(stringToMarkers(input) == expectedEquation);
 }
 
-TEST_CASE("parse math equations") {
-  SECTION("convert to string") {
-    std::list<Marker> equation = {
-      Marker::Digit_1,
-      Marker::Digit_0,
-      Marker::OperatorAdd,
-      Marker::Digit_7,
-      Marker::OperatorMultiply,
-      Marker::Digit_1,
-      Marker::OperatorSubtract,
-      Marker::Digit_8,
-      Marker::OperatorDivide,
-      Marker::Digit_3
-    };
-    auto tokens = parseTokens(equation);
-    REQUIRE(toString(tokens) == "10 + 7 * 1 - 8 / 3");
-  }
+TEST_CASE("convert tokens to string") {
+  std::list<Marker> equation = {
+    Marker::Digit_1,
+    Marker::Digit_0,
+    Marker::OperatorAdd,
+    Marker::Digit_7,
+    Marker::OperatorMultiply,
+    Marker::Digit_1,
+    Marker::OperatorSubtract,
+    Marker::Digit_8,
+    Marker::OperatorDivide,
+    Marker::Digit_3
+  };
+  auto tokens = parseTokens(equation);
+  REQUIRE(toString(tokens) == "10 + 7 * 1 - 8 / 3");
+}
 
-  SECTION("addition") {
-    std::list<Marker> equation = {
-      Marker::Digit_1,
-      Marker::Digit_0,
-      Marker::OperatorAdd,
-      Marker::Digit_9,
-      Marker::OperatorAdd,
-      Marker::Digit_5,
-      Marker::Digit_2,
-    };
-    auto ast = parseASTFromMarkers(equation);
-    REQUIRE(ast->value() == 71);
-  }
+int solveEquation(const std::string& input) {
+  auto ast = parseASTFromMarkers(stringToMarkers(input));
+  return ast->value();
+}
 
-  SECTION("subtraction") {
-    std::list<Marker> equation = {
-      Marker::Digit_9,
-      Marker::Digit_7,
-      Marker::OperatorSubtract,
-      Marker::Digit_1,
-      Marker::Digit_0
-    };
-    auto ast = parseASTFromMarkers(equation);
-    REQUIRE(ast->value() == 87);
-  }
-
-  SECTION("multiplication") {
-    std::list<Marker> equation = {
-      Marker::Digit_2,
-      Marker::OperatorAdd,
-      Marker::Digit_7,
-      Marker::OperatorMultiply,
-      Marker::Digit_3,
-      Marker::OperatorSubtract,
-      Marker::Digit_1,
-      Marker::Digit_0
-    };
-    auto ast = parseASTFromMarkers(equation);
-    REQUIRE(ast->value() == 13);
-  }
-
-  SECTION("division") {
-    std::list<Marker> equation = {
-      Marker::Digit_2,
-      Marker::Digit_2,
-      Marker::OperatorMultiply,
-      Marker::Digit_2,
-      Marker::OperatorDivide,
-      Marker::Digit_7
-    };
-    auto ast = parseASTFromMarkers(equation);
-    REQUIRE(ast->value() == 6);
-  }
+TEST_CASE("solve math equations") {
+  REQUIRE(solveEquation("10+9+52") == 71);
+  REQUIRE(solveEquation("97-10") == 87);
+  REQUIRE(solveEquation("2+7*3-10") == 13);
+  REQUIRE(solveEquation("22*2/7") == 6);
 }
