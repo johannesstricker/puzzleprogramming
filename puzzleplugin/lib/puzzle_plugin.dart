70a1025072ec6bd182f318249510b9859cf0a266
Johannes Stricker <stricker@bitside.de>
2021-08-15 13:46:24 +0200

    Add ffi structs for c bindings

--- puzzleplugin/lib/puzzle_plugin.dart
+++ puzzleplugin/lib/puzzle_plugin.dart
@@ -12,6 +12,61 @@ typedef DetectQrCode32BGRAFunction = Pointer<Utf8> Function(
 typedef TokenToStringFunctionNative = Pointer<Utf8> Function(Int32, Int32);
 typedef TokenToStringFunction = Pointer<Utf8> Function(int, int);
 
+// TODO: can we automatically keep this in sync with C++ enum?
+enum PuzzlePieceType {
+  digit0,
+  digit1,
+  digit2,
+  digit3,
+  digit4,
+  digit5,
+  digit6,
+  digit7,
+  digit8,
+  digit9,
+  add,
+  subtract,
+  multiply,
+  divide,
+  leftParenthesis,
+  rightParenthesis
+}
+
+class PuzzlePiece extends Struct {
+  @Int32()
+  external int typeIndex;
+
+  PuzzlePieceType type() {
+    return PuzzlePieceType.values[this.typeIndex];
+  }
+}
+
+class PuzzleSolution extends Struct {
+  @Int32()
+  external int piecesCount;
+
+  external Pointer<PuzzlePiece> pieces;
+
+  @Int32()
+  external int value;
+}
+
+class Challenge {
+  final int solution;
+  final List<PuzzlePieceType> availablePuzzlePieces;
+
+  Challenge(this.solution, this.availablePuzzlePieces);
+
+  bool checkSolution(PuzzleSolution solution) {
+    final remainingPieces = availablePuzzlePieces;
+    for (int i = 0; i < solution.piecesCount; i++) {
+      if (!remainingPieces.contains(solution.pieces[i])) ;
+      return false;
+    }
+    return solution.value == this.solution;
+  }
+}
+
 class PuzzlePlugin {
   static const MethodChannel _channel = const MethodChannel('puzzle_plugin');
 
