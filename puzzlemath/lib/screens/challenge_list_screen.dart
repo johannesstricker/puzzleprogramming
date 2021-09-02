56d356a90eafdc677abeb5d03a24fce197093c35
Johannes Stricker <stricker@bitside.de>
2021-09-02 07:18:53 +0200

    Extract challenges to separate file

--- puzzlemath/lib/screens/challenge_list_screen.dart
+++ puzzlemath/lib/screens/challenge_list_screen.dart
@@ -1,53 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:puzzlemath/math/challenge.dart';
-import 'package:puzzlemath/math/math.dart';
+import 'package:puzzlemath/math/challenge_repository.dart';
 import 'package:puzzlemath/widgets/challenge_list_item.dart';
 
 class ChallengeListScreen extends StatelessWidget {
   static const routeName = '/';
 
   final int progress;
-  final List<Challenge> challenges;
+  final List<Challenge> challenges = ChallengeRepository;
 
-  ChallengeListScreen({this.progress = 0})
-      : challenges = [
-          Challenge(0,
-              name: "What's the magic number?",
-              solution: 137,
-              availableMarkers: [Marker.Digit1, Marker.Digit3, Marker.Digit7]),
-          Challenge(
-            1,
-            name: "Child's play.",
-            solution: 2,
-            availableMarkers: [
-              Marker.Digit1,
-              Marker.Digit1,
-              Marker.OperatorAdd,
-            ],
-          ),
-          Challenge(2,
-              name: 'Easy peasy, lemon squeezy!',
-              solution: 28,
-              availableMarkers: [
-                Marker.Digit2,
-                Marker.Digit0,
-                Marker.OperatorAdd,
-                Marker.Digit1,
-                Marker.Digit2,
-                Marker.OperatorSubtract,
-                Marker.Digit4
-              ]),
-          Challenge(3,
-              name: "Let's turn these numbers up!",
-              solution: 999,
-              availableMarkers: [
-                Marker.Digit3,
-                Marker.Digit3,
-                Marker.Digit3,
-                Marker.Digit3,
-                Marker.OperatorMultiply,
-              ]),
-        ];
+  ChallengeListScreen({this.progress = 0});
 
   Widget buildChallengeItem(BuildContext context, int index) {
     final Challenge challenge = challenges[index];
